e61b9a2547075a22a54b5a431b60a81cbad1cb37
EmmilouPangasian-UCM <allebaemmilou@gmail.com>
2025-09-16 09:02:56 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,10 @@
+public class Comments {
+    public static void main(String[] args) {
+       
+        
+        System.out.println("Hello, this program shows different comments in Java.");
+       
+        
+        System.out.println("Comments help make code more readable.");
+    }
+}
---
+++ Chapter_1/TableAndChairs.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("This is line one of my favorite song,");
+        System.out.println("Here comes the second line, nice and strong.");
+        System.out.println("The third line keeps the rhythm flowing,");
+        System.out.println("And the fourth line keeps the music going!");
+    }
+}
